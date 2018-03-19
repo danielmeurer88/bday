0b828c54ab18aad35198a8dc09d0fe53467f65e7
Unknown <daniel@sestera.com>
2018-03-19 23:19:31 +0100

    mmh

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  template: `
+    <p>
+      <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 404 - page not found
+    </p>
+    <p>
+      <a routerLink="/bday">Wechsel zu bday</a>
+    </p>`,
+  styles: ['p {font-size:46px;}']
 })
 export class PageNotFoundComponent implements OnInit {
 
